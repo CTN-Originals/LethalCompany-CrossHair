6debcf370fce3f3aee632740d8798b9f47c9357d
CTN <info@ctn-originals.com>
2024-01-04 23:03:44 +0100

    small project improvement

---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CrossHair
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "CrossHair";
+        public const string PLUGIN_NAME = "CrossHair";
+        public const string PLUGIN_VERSION = "1.0.5";
+    }
+}
